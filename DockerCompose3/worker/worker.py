6bacb3266c4719a5305a8a60930ea6c702364c93
Karabasas <karabasas@DESKTOP-PJIGV5M>
2024-09-26 21:51:01 +0300

    Pataisymai trečiai užduočiai

--- DockerCompose3/worker/worker.py
+++ DockerCompose3/worker/worker.py
@@ -2,7 +2,7 @@ import time
 import redis
 
 # Connect to redis
-redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
+redis_client = redis.StrictRedis(host="dockercompose3-redis-1", port=6379, db=0)
 
 print("Worker started, listening for jobs")
 
