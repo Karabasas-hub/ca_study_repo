76e4b273d986d40d438e630170c63a39dff714e2
Karabasas <jurevicius99@gmail.com>
2024-10-19 13:27:50 +0300

    paiton3

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 def hello_world():
     return "Gal suveikė?"
 
+
 if __name__ == "__main__":
     print(hello_world())
+    
--- test_app.py
+++ test_app.py
@@ -1,10 +1,15 @@
 import unittest
 from app import hello_world
 
+
 class TestApp(unittest.TestCase):
+
+
     def test_hello_world(self):
+
+        
         self.assertEqual(hello_world(), "Gal suveiks?")
+
 
 if __name__ == "__main__":
     unittest.main()
-    
