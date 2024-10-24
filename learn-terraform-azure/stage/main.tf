558d340606c717f9a68b573ba384455910805e71
Karabasas <jurevicius99@gmail.com>
2024-10-24 20:14:15 +0300

    grazus

--- learn-terraform-azure/stage/main.tf
+++ learn-terraform-azure/stage/main.tf
@@ -4,11 +4,11 @@ variable environment {
 }
 
 module stage {
-    source = "./"
-    region = "northeurope"
-    resource_group_name = "${var.environment}-rsgp"
+    source               = "../modules"
+    region               = "northeurope"
+    resource_group_name  = "${var.environment}-rsgp"
     virtual_network_name = "${var.environment}-vpc"
-    subnet_name = "${var.environment}-subnet"
-    vm_name = "${var.environment}-catest001"
-    machine_size = "Standard_B1s"
+    subnet_name          = "${var.environment}-subnet"
+    vm_name              = "${var.environment}-catest001"
+    machine_size         = "Standard_B1s"
 }
--- learn-terraform-azure/test/main.tf
+++ learn-terraform-azure/test/main.tf
@@ -3,12 +3,12 @@ variable environment {
     default = "test"
 }
 
-module test {
-    source = "./"
-    region = "northeurope"
-    resource_group_name = "${var.environment}-rsgp"
+module test{
+    source               = "../modules"
+    region               = "northeurope"
+    resource_group_name  = "${var.environment}-rsgp"
     virtual_network_name = "${var.environment}-vpc"
-    subnet_name = "${var.environment}-subnet"
-    vm_name = "${var.environment}-catest001"
-    machine_size = "Standard_B1s"
+    subnet_name          = "${var.environment}-subnet"
+    vm_name              = "${var.environment}-catest001"
+    machine_size         = "Standard_B1s"
 }
