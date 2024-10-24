d0da75dcf5a0d23ca1bd11892ded40bd29146096
Karabasas <karabasas@DESKTOP-PJIGV5M>
2024-10-24 19:04:27 +0300

    b

--- learn-terraform-azure/main.tf
+++ learn-terraform-azure/main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "teralearn"
+  name     = var.name
   location = var.region
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "augustaststorage"
+  name                     = var.storage_name
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -12,27 +12,27 @@ resource "azurerm_storage_account" "example" {
 }
 
 resource "azurerm_storage_container" "example" {
-  name                  = "content"
+  name                  = var.container_name
   storage_account_name  = azurerm_storage_account.example.name
   container_access_type = "private"
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+  name                = var.network_name
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "example" {
-  name                 = "internal"
+  name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
+  name                = var.interface_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -44,7 +44,7 @@ resource "azurerm_network_interface" "example" {
 }
 
 resource "azurerm_virtual_machine" "example" {
-  name                = var.name
+  name                = var.vm_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_F2"
--- learn-terraform-azure/variables.tf
+++ learn-terraform-azure/variables.tf
@@ -7,4 +7,10 @@ variable name {
   type = string
 }
 
+var.resource_group_name
+var.virtual_network_name
+var.subnet_name
+var.vm_name
+var.machine_size
+
 
