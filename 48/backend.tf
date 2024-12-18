6afeb12c81c4c798dce37c78e9bd84cbba30f8a2
Karabasas <karabasas@DESKTOP-PJIGV5M>
2024-12-18 21:35:57 +0200

    vienuoliktas taisymas

--- 48/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket = "my-terraform-state-bucket"
-        key = "state-files/project/terraform.tfstate"
-        region = "eu-central-1"
-        dynamodb_table = "terraform-lock-table"
-    }
-}
